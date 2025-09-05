da77d60c7ddfcc265358e500fe3df3bb61f55ca8
Gabriel B.Poitras <gabrielbpoitras@gmail.com>
2025-09-05 15:33:53 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -16,8 +16,8 @@ from configurations import values
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("gabri", "gabrielbpoitras@gmail.com")]
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
