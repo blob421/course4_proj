4e034ffdf6cf683dce05a66d11384a1983c14680
Gabriel B.Poitras <gabrielbpoitras@gmail.com>
2025-09-06 17:28:57 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -42,6 +42,7 @@ class Dev(Configuration):
     # Application definition
 
     INSTALLED_APPS = [
+            "django_celery_beat",
          'django_celery_results',
         'movies.apps.MoviesConfig',
         'django.contrib.admin',
